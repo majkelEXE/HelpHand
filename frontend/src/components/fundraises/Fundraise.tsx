602af0caf041e27bf589f7da39a4ecd8485cec69
IgorBialek <igorbialek0@gmail.com>
2022-10-23 22:05:49 +0200

    Date fix

--- frontend/src/components/fundraises/Fundraise.tsx
+++ frontend/src/components/fundraises/Fundraise.tsx
@@ -42,7 +42,10 @@ const Fundraise = () => {
 
         <div className={css.location}>
           <h2>Kiedy i gdzie?</h2>
-          <h3>{new Date(fundraise.date).toLocaleString()}</h3>
+          <h3>
+            {new Date(fundraise.date).toLocaleDateString()},{" "}
+            {new Date(fundraise.date).toISOString().substring(11, 19)}
+          </h3>
           <h3>{fundraise.location.name}</h3>
         </div>
 
